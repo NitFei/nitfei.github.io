4ed61e79eef762e97a810a36492c3f2fdf2e8981
NitFei <feigenba@uni-hildesheim.de>
2021-07-26 17:42:18 +0200

    sdf

--- digitext/scrolly/js/main.js
+++ digitext/scrolly/js/main.js
@@ -3,23 +3,27 @@ const tl = gsap.timeline({
         trigger: '.animation',
         pin: true,
         start: 'top top',
-        end: '+=5000',
+        end: '+=10000',
         scrub: 1.5,
     }
 });
 
 
 tl.addLabel('tree001')
-    .to('#tree001', {duration: 1, x:-window.innerWidth, ease: 'none'},0)
+    .to('#tree001', {duration: 5, x:-2*window.innerWidth, ease: 'none'},0)
     .addLabel('tree002')
-    .to('#tree002', {duration: 1, x:-window.innerWidth, ease: 'none'},0.5)
-    .addLabel('highlights')
-    .fromTo('.highlights', {opacity: 0}, {duration: 0.5, opacity: 0.4}, 0)
-    .fromTo('.highlights', {opacity: 0.4}, {duration: 0.5, opacity: 0.7}, 2)
-    .addLabel('lowlights')
-    .fromTo('.lowlights', {opacity: 0}, {duration: 0.5, opacity: 0.4}, 0)
-    .fromTo('.lowlights', {opacity: 0.4}, {duration: 0.5, opacity: 0.7}, 2)
+    .to('#tree002', {duration: 6, x:-2*window.innerWidth, ease: 'none'},0.5)
+    .addLabel('face1')
+    .fromTo('.face1', {opacity: 0}, {duration: 1.5, opacity: 0.3}, 0)
+    .fromTo('.face1', {opacity: 0.3}, {duration: 1.5, opacity: 0.5}, 2)
+    .fromTo('.face1', {opacity: 0.5}, {duration: 1.5, opacity: 0.7}, 4)
+    .addLabel('face2')
+    .fromTo('.face2', {opacity: 0.2}, {duration: 1.5, opacity: 0.4}, 2)
+    .fromTo('.face2', {opacity: 0.4}, {duration: 1.5, opacity: 0.8}, 3)
+    .addLabel('face3')
+    .fromTo('.face3', {opacity: 0.2}, {duration: 1.5, opacity: 0.8}, 3)
+    .fromTo('.face3', {opacity: 0.8}, {duration: 1.5, opacity: 0.4}, 4)
     .addLabel('text')
-    .fromTo('#text001', {x: 0}, {duration: 1, x: -1000}, 1)
-    .fromTo('#text001', {x: -1000}, {duration: 3, x:-3000}, 3)
+    .fromTo('#text001', {x: 0}, {duration: 1, x: -1000}, 2)
+    .fromTo('#text001', {x: -1000}, {duration: 3, x:-3000}, 5)
     .addLabel('end');    
