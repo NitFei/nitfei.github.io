8cd093bc779f588ec987bf16f4c1ee4834bae416
NitFei <feigenba@uni-hildesheim.de>
2021-07-26 13:19:24 +0200

    qasdfd

--- digitext/scrolly/js/main.js
+++ digitext/scrolly/js/main.js
@@ -1,26 +1,25 @@
-const flightPath = {
-    curviness: 1.25,
-    values: [
-        {x:-window.innerWidth, y: 0}
-    ]
-}
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.animation',
+        pin: true,
+        start: 'top top',
+        end: '+=5000',
+        scrub: 1.5,
+    }
+});
 
-const tween = new TimelineLite();
 
-tween.add(
-    TweenLite.to('.tree', 1, {
-        bezier: flightPath,
-        ease: Power1.easeInOut
-    })
-);
-
-const controller = new ScrollMagic.Controller();
-
-const scene = new ScrollMagic.Scene({
-    triggerElement: '.animation',
-    duration: 1000,
-    triggerHook: 0
-})
-.setTween(tween)
-.setPin('.animation')
-.addTo(controller);
+tl.addLabel('tree001')
+    .to('#tree001', {duration: 1, x:-window.innerWidth, ease: 'none'},0)
+    .addLabel('tree002')
+    .to('#tree002', {duration: 1, x:-window.innerWidth, ease: 'none'},0.5)
+    .addLabel('highlights')
+    .fromTo('.highlights', {opacity: 0}, {duration: 0.5, opacity: 0.4}, 0)
+    .fromTo('.highlights', {opacity: 0.4}, {duration: 0.5, opacity: 0.7}, 2)
+    .addLabel('lowlights')
+    .fromTo('.lowlights', {opacity: 0}, {duration: 0.5, opacity: 0.4}, 0)
+    .fromTo('.lowlights', {opacity: 0.4}, {duration: 0.5, opacity: 0.7}, 2)
+    .addLabel('text')
+    .fromTo('#text001', {x: 0}, {duration: 1, x: -1000}, 1)
+    .fromTo('#text001', {x: -1000}, {duration: 3, x:-3000}, 3)
+    .addLabel('end');    
