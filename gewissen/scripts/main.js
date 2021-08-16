6d443c8986740780debd6bf547150c89c8ce2f4f
NitFei <feigenba@uni-hildesheim.de>
2021-08-16 15:27:54 +0200

    lel

--- gewissen/scripts/main.js
+++ gewissen/scripts/main.js
@@ -6,10 +6,7 @@ $(document).ready(function(){
     });
 
     $('.back').click(function() {
-        makeFrontBig($('.front'), $(this));
-
-        bringBackToFront($(this));
-        $(this).off('click');
+        handleBackClick($(this));
     })
 })
 
@@ -19,6 +16,7 @@ function resizeBack() {
 }
 
 function bringBackToFront(back) {
+    back.removeClass('animate');
     if(back.hasClass('devil-wrapper')){
         back.css({'right': '0', 'top': ''});
     } else {
@@ -34,7 +32,7 @@ function makeFrontBig(front, devAng) {
     const w = front.width();
 
     if(devAng.hasClass('devil-wrapper')){
-        // devil needs to move tuff to the left
+        // devil needs to move stuff to the left
         front.css({'right': '0', 'left':''});
         front.animate({'width': 4 * w + 'px', 'top': 4 * -w * 0.15 + 'px', 'right': 4 * -w * 0.2 + 'px', 'opacity': 0}, 500, function() {moveFrontToBack(front, devAng)});
     } else {
@@ -48,52 +46,84 @@ function moveFrontToBack(front, devAng) {
     front.empty();
     front.removeClass();
     front.addClass('back');
+    front.addClass('opacity0');
     front.removeAttr("style");
 
     if(devAng.hasClass('devil-wrapper')){
         front.addClass('devil-wrapper');
-
-        // add head
-        const head = $('<img>');
-        head.prop('src','../src/devils/heads/head001.png');
-        head.appendTo(front);
-
-        // add torso
-        const torso = $('<img>');
-        torso.prop('src','../src/devils/torsos/torso001.png');
-        torso.appendTo(front);
-
-        // add legs
-        const legs = $('<img>');
-        legs.prop('src','../src/devils/legs/legs001.png');
-        legs.appendTo(front);
+        addBodyImages(front, '../src/devils/');
     } else {
         front.addClass('angel-wrapper');
-
-        const head = $('<img>');
-        head.prop('src','../src/angels/heads/head001.png');
-        head.appendTo(front);
-
-        // add torso
-        const torso = $('<img>');
-        torso.prop('src','../src/angels/torsos/torso001.png');
-        torso.appendTo(front);
-
-        // add legs
-        const legs = $('<img>');
-        legs.prop('src','../src/angels/legs/legs001.png');
-        legs.appendTo(front);
+        addBodyImages(front, '../src/angels/');
     }
 
-    
-
-    
-
     front.click(function() {
-        makeFrontBig($('.front'), $(this));
-        bringBackToFront($(this));
-        $(this).off('click');
+        handleBackClick(front)
     })
 
     resizeBack();
+
+    front.addClass('animate');
+    front.removeClass('opacity0');
+}
+
+function addBodyImages(front, path) {
+    // add head
+    const head = createBodyPart(path + 'heads/head001.png');
+    head.appendTo(front);
+
+    // add torso
+    const torso = createBodyPart(path + 'torsos/torso001.png');
+    torso.appendTo(front);
+
+    // add legs
+    const legs = createBodyPart(path + 'legs/legs001.png');
+    legs.appendTo(front);
+}
+
+function createBodyPart(path) {
+    const part = $('<img>');
+    part.prop('src', path);
+    return part;
+}
+
+function moveOtherAside(other) {
+    other.removeClass('animate');
+    const w = $('.front').width();
+
+    if(other.hasClass('devil-wrapper')){
+        // devil needs to be moved to the right
+        other.animate({'width': w * 0.8, 'top': 0, 'right': 7 * -w * 0.2 + 'px', 'opacity': 0}, 500, function() {resetToBack(other)});
+    } else {
+        // angel moves to the left
+        other.animate({'width': w * 0.8, 'top': 0, 'left': 7 *  -w * 0.2 + 'px', 'opacity': 0}, 500, function() {resetToBack(other)});
+    }
+}
+
+function resetToBack(other) {
+    other.addClass('opacity0')
+    other.removeAttr('style');
+
+
+    resizeBack();
+
+    other.addClass('animate');
+    other.removeClass('opacity0');
+}
+
+function handleBackClick(me) {
+    let other;
+    if(me.hasClass('devil-wrapper')){
+        other = $('.back.angel-wrapper');
+    } else {
+        other = $('.back.devil-wrapper');
+    }
+
+    moveOtherAside(other);
+
+    makeFrontBig($('.front'), me);
+
+
+    bringBackToFront(me);
+    me.off('click');
 }
